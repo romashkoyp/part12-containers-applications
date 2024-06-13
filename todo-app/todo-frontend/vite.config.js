0632551c7e19eae6bf645a53808e19dbb609584b
Yaroslav <romashkoyp@gmail.com>
2024-06-13 16:19:11 +0300

    12.15

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all available network interfaces
+    hmr: {
+      overlay: true, // Show full-screen errors during development
+    },
+  },
+});
